5d477a7c7b71eb895d0f4caac8d3cb3aa984c7a2
OleksiiDavydiuk <Oleksii.Davydyuk@gmail.com>
2016-09-21 18:23:21 +0300

    init

---
+++ ImageDataBase/ImageDataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageData.Entitys;
+
+namespace ImageData
+{
+    public class ImageDataContext: DbContext
+    {
+        public ImageDataContext(): base("ImageDataBase")
+        {
+            Database.SetInitializer<ImageDataContext>( new ImageDataInitializer());
+        }
+
+        public DbSet<ImageEntity> ImageEntitys { get; set; }
+    }
+}
