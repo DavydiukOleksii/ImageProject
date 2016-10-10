595a1f33e4a03ec67326690e5d22239e791b79e5
OleksiiDavydiuk <Oleksii.Davydyuk@gmail.com>
2016-10-10 15:22:22 +0300

    add button

--- WpfClient/ViewModel/MainWindowViewModel.cs
+++ WpfClient/ViewModel/MainWindowViewModel.cs
@@ -52,31 +52,83 @@ namespace WpfClient.View.ViewModel
         }
         #endregion
 
-        #region ButtonCommand
-        //RelayCommand _clientCommand;
+        #region Command
 
-        //public ICommand Client
-        //{
-        //    get
-        //    {
-        //        if (_clientCommand == null)
-        //        {
-        //            _clientCommand = new RelayCommand(ExecuteAddImageCommand, CanExecuteAddImageCommand);
-        //        }
-        //        return _clientCommand;
-        //    }
-        //}
+        #region NextImage
+        RelayCommand _nextImageCommand;
+        
+        public ICommand NextImage
+        {
+            get
+            {
+                if (_nextImageCommand == null)
+                {
+                    _nextImageCommand = new RelayCommand(ExecuteNextImageCommand, CanExecuteNextImageCommand);
+                }
+                return _nextImageCommand;
+            }
+        }
 
-        //public void ExecuteAddImageCommand(object parameter)
-        //{
-        //    Images.Add(new ImageViewModel { Description = "add image"});
-        //}
+        public void ExecuteNextImageCommand(object parameter)
+        {
+            if (SelectedImage.ImageId != Images.Count())
+            {
+                SelectedImage = Images.ElementAt(SelectedImage.ImageId);
+            }
+            else
+            {
+                SelectedImage = Images.First();
+            }
+        }
 
-        //public bool CanExecuteAddImageCommand(object parameter)
-        //{
-        //    if(true) return true;
-        //    else return false;
-        //}
+        public bool CanExecuteNextImageCommand(object parameter)
+        {
+            if (Images != null)
+            {
+                return true;
+            }
+            else return false;
+        }
+        #endregion
+
+        #region PreviousImage
+        RelayCommand _prevImageCommand;
+
+        public ICommand PrevImage
+        {
+            get
+            {
+                if (_prevImageCommand == null)
+                {
+                    _prevImageCommand = new RelayCommand(ExecutePrevImageCommand, CanExecutePrevImageCommand);
+                }
+                return _prevImageCommand;
+            }
+        }
+
+        public void ExecutePrevImageCommand(object parameter)
+        {
+            if (SelectedImage.ImageId != 1)
+            {
+                SelectedImage = Images.ElementAt(SelectedImage.ImageId - 2);
+            }
+            else
+            {
+                SelectedImage = Images.Last();
+            }
+            
+        }
+
+        public bool CanExecutePrevImageCommand(object parameter)
+        {
+            if (Images != null)
+            {
+                return true;
+            }
+            else return false;
+        }
+        #endregion
+
         #endregion
 
         #region FreeData
